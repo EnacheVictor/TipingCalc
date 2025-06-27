328a3e36197f2fb163c358ff0742550084cfe4ff
Victor Enache <enachevictor887@gmail.com>
2025-06-27 16:51:00 +0300

    Initial commit

---
+++ app/src/main/java/com/example/tipingapp/MainActivity.kt
@@ -0,0 +1,108 @@
+package com.example.tipingapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.tipingapp.components.InputField
+import com.example.tipingapp.ui.theme.TipingAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TipingApp{
+                MainContent()
+            }
+        }
+    }
+}
+
+@Composable
+fun TipingApp(content: @Composable () -> Unit){
+
+    Surface(color = MaterialTheme.colorScheme.background) {
+        content()
+    }
+}
+@Preview
+@Composable
+fun TopHeader(totalPerPerson: Double = 134.0){
+    Surface(modifier = Modifier.fillMaxWidth()
+        .height(150.dp)
+        .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp))),
+        color = Color(0xFFE9D7F7)
+    ) {
+        Column(modifier = Modifier.padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center) {
+            val total = "%.2f".format(totalPerPerson)
+            Text(text = "Total per person:",
+                style = MaterialTheme.typography.titleMedium)
+            Text(text = "$total$",
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.Bold)
+        }
+    }
+}
+@Preview
+@Composable
+fun MainContent(){
+    
+}
+
+@Composable
+fun BillForm(modifier: Modifier = Modifier,
+             onValChange: (String) -> Unit = {}){
+    val totalBillState = remember {
+        mutableStateOf("")
+    }
+    val validState = remember(totalBillState.value) {
+        totalBillState.value.trim().isNotEmpty()
+    }
+    val keyboardController = LocalSoftwareKeyboardController.current
+    Surface(modifier = Modifier.padding(2.dp).fillMaxWidth(),
+        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        border = BorderStroke(width = 1.dp , color = Color.LightGray)
+    ) {
+        Column() {
+            InputField(valueState = totalBillState,
+                labelId = "Enter Bill",
+                enabled = true,
+                isSingleLine = true,
+                onAction = KeyboardActions{
+                    if(!validState) return@KeyboardActions
+                    onValChange(totalBillState.value.trim())
+                    keyboardController?.hide()
+                }
+            )
+        }
+    }
+}
